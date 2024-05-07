973d660d06f0d37302817c54446500b98c00f71e
Esi Yankey <graceyan244@gmail.com>
2024-05-07 08:29:09 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { welcomePage } from './components/welcomePage'
+
+function App() {
+ 
+  return (
+    <>
+      <welcomePage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/welcomePage.jsx
@@ -0,0 +1,8 @@
+import {useEffect,useState} from 'react'
+
+export const welcomePage = () => {
+   
+  return (
+    <div> My Book <br/> Shelf </div>
+  )
+}
