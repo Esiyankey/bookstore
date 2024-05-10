5ba8d6db61fd85685cccf19308c5384c282ed05f
Esi Yankey <graceyan244@gmail.com>
2024-05-10 15:47:07 +0000

    worked on api

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react";
 import image from "../assets/aroundImage.png";
-import '../styles/books.css'
+import "../styles/books.css";
 export const Books = () => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
     const fetchBooks = async () => {
       try {
-        const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&key=AIzaSyCtyMhzEs0GwNdNV2MqeafsDIhgRcW-N3Y');
+        const response = await fetch(
+          "https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&key=AIzaSyCtyMhzEs0GwNdNV2MqeafsDIhgRcW-N3Y"
+        );
         const data = await response.json();
         console.log("Fetched data:", data.items);
         setBooks(data.items);
@@ -22,18 +24,23 @@ export const Books = () => {
   return (
     <>
       <div className="books">
-      {books.map((book, index) => (
-        <div className="book" key={index}>
-          <div className="book-image">
-            <img className="img" src={image} alt="image" />
-          </div>
-          <div className="title">{book.title}</div>
-          <div className="author">{book.author}</div>
-          <div className="rating">Rating: {book.rating}</div>
-        </div>
-      ))}
-       {books.length === 0 && <p>No books found.</p>}
-    </div>
+        {books.map((book, index) => {
+        const imageUrl = book.volumeInfo?.imageLinks?.smallThumbnail;
+        const title = book.volumeInfo.title;
+        const author = book.volumeInfo.authors?.[0];
+          return (
+            <div className="book" key={index}>
+              <div className="book-image">
+                <img className="img" src={imageUrl} alt="image" />
+              </div>
+              <div className="title">{title}</div>
+              <div className="author">{author}</div>
+              {/* <div className="rating">Rating: {book.rating}</div> */}
+            </div>
+          );
+        })}
+        {books.length === 0 && <p>No books found.</p>}
+      </div>
     </>
   );
 };
--- src/components/Search_Bar.jsx
+++ src/components/Search_Bar.jsx
@@ -4,39 +4,46 @@ import { FaStaylinked } from "react-icons/fa6";
 import { GiBookshelf } from "react-icons/gi";
 import image from "../assets/aroundImage.png";
 import { IoIosSearch } from "react-icons/io";
-import { CgHello } from "react-icons/cg";
+
 
 export const Search_Bar = () => {
-  const [search,setSearch]=useState("")
-  const searchBook =()=>{
-    if (event.key === 'Enter') {
-      console.log("hello")
-      setSearch("")
+  const [search, setSearch] = useState("");
+  const searchBook = () => {
+    if (event.key === "Enter") {
+      console.log("hello");
+      setSearch("");
     }
-  }
-  const handleClick=()=>{
-    console.log("clicked")
-    setSearch("")
-  }
+  };
+  const handleClick = () => {
+    console.log("clicked");
+    setSearch("");
+  };
   return (
     <div className="main-searchbar">
       <div className="search">
-        <div className="all">
-          {" "}
-          All <GoTriangleDown />
-        </div>
+        <div className="all">All</div>
         <div className="search-space">
-
-        <input  type="text" className="input-search" value={search} onChange={e=>setSearch(e.target.value)} onKeyDown={searchBook} placeholder="Search" />
-        <IoIosSearch className="searchbar-icons" onClick={handleClick} style={{fontSize:"20px"}} />
+          <input
+            type="text"
+            className="input-search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyDown={searchBook}
+            placeholder="Search"
+          />
+          <IoIosSearch
+            className="searchbar-icons"
+            onClick={handleClick}
+            style={{ fontSize: "20px" }}
+          />
         </div>
-        
+
         <div className="dot">
-          <FaStaylinked className="searchbar-icons"/>
+          <FaStaylinked className="searchbar-icons" />
         </div>
       </div>
       <div className="categories">
-        <GiBookshelf className="searchbar-icons"/>
+        <GiBookshelf className="searchbar-icons" />
         Categories
         <GoTriangleDown />
       </div>
